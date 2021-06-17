2fae4f13266b486c7968dd9d47923e33c02f5298
Sagar Shanbhag <sagar.shanbhag27@gmail.com>
2021-06-18 00:45:30 +0530

    Login Page created

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class LoginPage extends StatefulWidget {
   static const String id = 'login_page';
@@ -9,6 +10,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,36 +26,157 @@ class _LoginPageState extends State<LoginPage> {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Container(
-                height: 700,
+                height: 650,
                 width: 550,
               child: Card(
                 shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.blue.shade900, width: 1),
+                  side: BorderSide(color: HexColor("#01274a"), width: 1),
                   borderRadius: BorderRadius.circular(11),
                 ),
-                  color: Colors.blue.shade900,
-                 child: Column(
-                   children: <Widget>[
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Image.asset(
-                          'images/handshake2.png',
-                         height: 200,
-                        width: 200,
-                      ),
-                    ),
-                    Text(
-                         'Sahaayak',
-                         style: TextStyle(
-                             fontFamily: 'Ruluko',
-                            color: Colors.white,
-                           fontSize: 60,
+                  color: HexColor("#01274a"),
+                 child: Center(
+                   child: Container(
+                     width: 300,
+                     child: Column(
+                       children: <Widget>[
+                         SizedBox(
+                           height: 10,
                          ),
+                        Image.asset(
+                              'images/handshake2.png',
+                             height: 180,
+                            width: 200,
+                          ),
+                        Text(
+                             'Sahaayak',
+                             style: TextStyle(
+                                 fontFamily: 'Ruluko',
+                                color: Colors.white,
+                               fontSize: 60,
+                             ),
+                         ),
+                         SizedBox(
+                           height: 25,
+                         ),
+                         TextField(
+                           style: TextStyle(color: Colors.white,fontFamily: 'Ruluko'),
+                           decoration: InputDecoration(
+                             border: OutlineInputBorder(
+                               borderRadius: BorderRadius.all(Radius.circular(11.0)),
+                             ),
+                             labelText: 'Email',
+                              labelStyle: TextStyle(
+                                  color: Colors.blueGrey,
+                                 fontFamily: 'Ruluko',
+                                fontWeight: FontWeight.bold,
+                              ),
+                              hoverColor: Colors.white,
+                            ),
+                         ),
+                         SizedBox(
+                           height: 30,
+                         ),
+                         TextField(
+                           obscureText: true,
+                           style: TextStyle(color: Colors.white,fontFamily: 'Ruluko'),
+                           decoration: InputDecoration(
+                             border: OutlineInputBorder(
+                               borderRadius: BorderRadius.all(Radius.circular(11.0)),
+                             ),
+                             labelText: 'Password',
+                             labelStyle: TextStyle(
+                               color: Colors.blueGrey,
+                               fontFamily: 'Ruluko',
+                               fontWeight: FontWeight.bold,
+                             ),
+                             hoverColor: Colors.white,
+                           ),
+                         ),
+                         SizedBox(
+                           height: 10,
+                         ),
+                         Row(
+                           children: <Widget>[
+                             Checkbox(
+                               checkColor: Colors.white,
+                               value: isChecked,
+                               onChanged: (bool? value) {
+                                 setState(() {
+                                   isChecked = value!;
+                                 });
+                               },
+                             ),
+                             SizedBox(
+                               width: 6,
+                             ),
+                             Text(
+                                 'Remember Me',
+                                 style: TextStyle(
+                                   color: Colors.white,
+                                   fontFamily: 'Ruluko',
+                                   fontSize: 20,
+                                 ),
+                             )
+                           ],
+                         ),
+                         SizedBox(
+                           height: 25,
+                         ),
+                         ConstrainedBox(
+                           constraints: BoxConstraints.tightFor(width: 250, height: 40),
+                           child: ElevatedButton(
+                             style: ElevatedButton.styleFrom(
+                               primary: HexColor("#007ef2"),
+                               onPrimary: Colors.white,
+                               shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+                               textStyle: TextStyle(
+                                 fontFamily: 'Ruluko',
+                                 fontSize: 20
+                               ),
+                               shadowColor: Colors.black
+                             ),
+                             onPressed: () {},
+                             child: const Text('Submit'),
+                           ),
+                         ),
+                         SizedBox(
+                           height: 20,
+                         ),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                           children: <Widget>[
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 textStyle: const TextStyle(
+                                     fontSize: 20,
+                                      fontFamily: 'Ruluko',
+                                      decoration: TextDecoration.underline
+                                 ),
+                                 primary: Colors.white,
+                               ),
+                               onPressed: () {},
+                               child: const Text('New User'),
+                             ),
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 textStyle: const TextStyle(
+                                     fontSize: 20,
+                                     fontFamily: 'Ruluko',
+                                     decoration: TextDecoration.underline
+                                 ),
+                                 primary: Colors.white,
+                               ),
+                               onPressed: () {},
+                               child: const Text('Forgot Password?'),
+                             ),
+                           ],
+                         ),
+                         SizedBox(
+                           height: 10,
+                         ),
+                       ],
                      ),
-                     
-                   ],
+                   ),
                  ),
                 ),
                 decoration: new BoxDecoration(
@@ -61,10 +184,10 @@ class _LoginPageState extends State<LoginPage> {
                     new BoxShadow(
                       color: Colors.black,
                       offset: const Offset(
-                        5.0,
-                        5.0,
+                        3.0,
+                        3.0,
                       ),
-                      blurRadius: 10.0,
+                      blurRadius: 6.0,
                       spreadRadius: 2.0
                     )
                   ]
